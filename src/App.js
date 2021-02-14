19a2a23c03193bd39b2feac5539b527779f423a1
Avshalom <avshalom.tam@gmail.com>
2021-02-14 22:13:31 +0200

    styled application

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import './app.css'
 import axios from 'axios'
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
   const categoryEl = useRef()
+  const amountEl = useRef()
 
   useEffect(() => {
     axios
     .get('https://opentdb.com/api_category.php')
     .then(response => {
       const {data} = response
-      console.log(data.trivia_categories)
+      setCategories(data.trivia_categories)
     })
   }, [])
 
   useEffect(() => {
+    
+  }, [])
+
+  // this function converts all html chars inside the questions to normal string
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
     axios
-    .get('https://opentdb.com/api.php?amount=10')
+    .get('https://opentdb.com/api.php', {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
     .then((response) => {
       const {data} = response
       const {results} = data 
@@ -41,17 +60,6 @@ function App() {
 
       setFlashcards(properQuestions)
     })
-  }, [])
-
-  // this function converts all html chars inside the questions to normal string
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault()
   }
 
   return (
@@ -60,7 +68,27 @@ function App() {
       <div className="form-group">
         <label htmlFor="category">Category</label>
         <select id="category" ref={categoryEl}>
+          {categories.map(category => {
+            return <option value={category.id} key={category.id}>{category.name}</option>
+          })}
         </select>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="amount">Amount</label>
+        <input 
+          type="number" 
+          id="amount" 
+          min="1" 
+          step="1"
+          defaultValue={10}
+          ref={amountEl}
+           >
+        </input>
+      </div>
+
+      <div className="form-group">
+          <button className="btn">Generate</button>
       </div>
     </form>
     
@@ -70,39 +98,5 @@ function App() {
     </>
   );
 }
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: 'what is 2+2?',
-    answer: '4',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  }, {
-    id: 2,
-    question: 'what is 2+5?',
-    answer: '7',
-    options: [
-      '7',
-      '3',
-      '4',
-      '5'
-    ]
-  }, {
-    id: 3,
-    question: 'what is 2+10?',
-    answer: '12',
-    options: [
-      '2',
-      '12',
-      '4',
-      '5'
-    ]
-  }
-]
 
 export default App;
