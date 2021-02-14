a7a8dc90c40b522dd9cd35512be44ece9a3cdf56
Avshalom <avshalom.tam@gmail.com>
2021-02-14 21:07:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import FlashcardList from "./FlashcardList";
+import './app.css'
+import axios from 'axios'
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+  useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then((response) => {
+      const {data} = response
+      const {results} = data 
+      const properQuestions = results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const fourOptions = [
+          answer,
+           ...questionItem.incorrect_answers.map(ans => {
+             return decodeString(ans)
+           })
+          ]
+
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: fourOptions.sort(() => Math.random() - .5)
+        }
+      })
+
+      setFlashcards(properQuestions)
+    })
+  }, [])
+
+// this function converts all html chars inside the questions to normal string
+function decodeString(str) {
+  const textArea = document.createElement('textarea')
+  textArea.innerHTML = str
+  return textArea.value
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FlashcardList flashcards={flashcards}/>
     </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'what is 2+2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }, {
+    id: 2,
+    question: 'what is 2+5?',
+    answer: '7',
+    options: [
+      '7',
+      '3',
+      '4',
+      '5'
+    ]
+  }, {
+    id: 3,
+    question: 'what is 2+10?',
+    answer: '12',
+    options: [
+      '2',
+      '12',
+      '4',
+      '5'
+    ]
+  }
+]
 
 export default App;
